16ad9f7b5fdfce3d1b3fbdae270c5937240127e2
zulidafarina <orien31.farina@gmail.com>
2021-04-03 16:32:28 +0800

    Buat Output

--- InputOutput.java
+++ InputOutput.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
+  String nama;
+  System.out.println("Masukkan nama anda: ");
+  nama = scanner.nextLine();
+  System.out.println("Hai, "+nama);
 
 }
 }
