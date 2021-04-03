6191c9244c1526cd129f52d848978e99d2743128
zulidafarina <orien31.farina@gmail.com>
2021-04-03 15:51:56 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+ System.out.println();
+ System.out.println("== Data Mahasiswa ====================");
+ System.out.println("NPM  : 19630916 " );
+ System.out.println("Nama : Zulida Farina");
+ System.out.println("==================================");
+}
+}
+ 
---
+++ model/MataKuliah.java
@@ -0,0 +1,12 @@
+package model;
+
+public class MataKuliah{
+ public static void tampilanInfo(){
+ System.out.println();
+ System.out.println("== Data Mata Kuliah ====================");
+ System.out.println("Kode : TIF3402 " );
+ System.out.println("Nama : Pemrograman Berbasis Objek");
+ System.out.println("==================================");
+}
+}
+ 
